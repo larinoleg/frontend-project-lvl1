b4c2e531f929197369a4c0a60f3b767337e8d535
oleg <oleglorin@gmail.com>
2021-12-07 15:34:22 +0300

    brain-pr.js commit fixes 3

--- bin/brain-pr.js
+++ bin/brain-pr.js
@@ -8,18 +8,20 @@ greet();
 while (k < 3) {
   let i = 0;
   let j = 2;
-  let chislo = Math.floor(Math.random()*10+1);
+  const chislo = Math.floor(Math.random() * 10 + 1);
 
   while (j < 9) {
-    if (chislo % j === 0 && j !== chislo) { i++; }
-      j += 1;
-    }
+    if (chislo % j === 0 && j !== chislo) { i += 1; }
+    j += 1;
+  }
 
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
   console.log(`Qestion: ${chislo}`);
   console.log(`i is ${i}`);
   const answ = r.question('Answer: ');
-  if ((answ === "yes" && i === 0)|| (answ === "no" && i !== 0) || (chislo === 2 && answ === "yes")) { console.log('right'); }
-  else { console.log('wrong'); break; }
+  if ((answ === 'yes' && i === 0)|| (answ === 'no' && i !== 0) || (chislo === 2 && answ === 'yes')) 
+  { console.log('right'); }
+  else 
+  { console.log('wrong'); break; }
   k += 1;
 }
