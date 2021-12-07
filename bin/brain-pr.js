1148d7d1f2508162ed423e618b0b93b82f36dbe9
oleg <oleglorin@gmail.com>
2021-12-07 15:32:10 +0300

    brain-pr.js commit fixes 2

--- bin/brain-pr.js
+++ bin/brain-pr.js
@@ -1,29 +1,25 @@
 #!/usr/bin/env node
 import r from 'readline-sync';
-import {greet} from './greet.js';
-
-
+import { greet } from './greet.js';
 
 let k = 0;
 greet();
 
 while (k < 3) {
-    let i = 0;
-    let j = 2;
+  let i = 0;
+  let j = 2;
+  let chislo = Math.floor(Math.random()*10+1);
 
-
-    let chislo = Math.floor(Math.random()*10+1);
-
-    while (j < 9) {
-        if (chislo % j === 0 && j !== chislo) { i++; }
-        j+=1;
+  while (j < 9) {
+    if (chislo % j === 0 && j !== chislo) { i++; }
+      j += 1;
     }
 
-    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-    console.log(`Qestion: ${chislo}`);
-    console.log(`i is ${i}`);
-    const answ = r.question('Answer: ');
-    if (answ === "yes" && i === 0 || answ === "no" && i !== 0 || chislo === 2 && answ === "yes") { console.log('right'); }
-    else { console.log('wrong'); break; }
-    k+=1;
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  console.log(`Qestion: ${chislo}`);
+  console.log(`i is ${i}`);
+  const answ = r.question('Answer: ');
+  if ((answ === "yes" && i === 0)|| (answ === "no" && i !== 0) || (chislo === 2 && answ === "yes")) { console.log('right'); }
+  else { console.log('wrong'); break; }
+  k += 1;
 }
